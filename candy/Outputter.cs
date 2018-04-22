b33775bd7a1e6339324a295eba8b423e4421640d
Mattias Örtenblad <mattias.ortenblad@gmail.com>
2018-04-22 15:52:24 +0200

    basic ascii game boilerplate

---
+++ candy/Outputter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+
+namespace candy
+{
+    public class Outputter
+    {
+        private readonly int width;
+        private readonly int height;
+
+        public Outputter(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        /// <summary>
+        /// Write a character to console, return cursor to initial position
+        /// </summary>
+        public void WC(char c)
+        {
+            var top = Console.CursorTop;
+            var left = Console.CursorLeft;
+            Console.Write(c);
+            Set(left, top);
+        }
+
+        /// <summary>
+        /// Move the cursor
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="top"></param>
+        public void Set(int left, int top)
+        {
+            Console.SetCursorPosition(left, top);
+        }
+        
+        /// <summary>
+        /// Move the cursor from its current position
+        /// </summary>
+        /// <param name="addLeft"></param>
+        /// <param name="addTop"></param>
+        public void MoveFromTop(int steps)
+        {
+            var newPositionTop = Console.CursorTop + steps;
+
+            if (newPositionTop < 0) newPositionTop = 0;
+            if (newPositionTop >= height) newPositionTop = height -1;
+
+            Console.CursorTop = newPositionTop;
+            //Console.SetCursorPosition(Console.CursorLeft, newPositionTop);
+        }
+        
+        
+        /// <summary>
+        /// Move the cursor from its current position
+        /// </summary>
+        /// <param name="addLeft"></param>
+        /// <param name="addTop"></param>
+        public void MoveFromLeft(int steps)
+        {
+            var newPositionLeft = Console.CursorLeft + steps;
+            
+            
+            if (newPositionLeft < 0) newPositionLeft = 0;
+            if (newPositionLeft >= width) newPositionLeft = width - 1;
+
+            Console.CursorLeft = newPositionLeft;
+            
+            //Console.SetCursorPosition(newPositionLeft, Console.CursorTop);
+        }
+        
+        
+        
+
+        public void InitCanvas()
+        {
+            
+            Console.WindowWidth = width;
+            Console.WindowHeight = height;
+            Console.CursorVisible = false;
+            
+            
+            for (int left = 0; left < width; left++)
+            {
+                int top;
+                for (top = 0; top < height; top++)
+                {
+                    //Thread.Sleep(5);
+                    Set(left, top);
+                    WC('*');
+                }
+            }
+            
+            Set(5,5);
+        }
+    }
+}
---
+++ candy/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace candy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var height = 30;
+            var width = 50;
+            
+            var op = new Outputter(width, height);
+            op.InitCanvas();
+
+            while (true)
+            {
+                var keyPress = GetUserInput(op);
+                MoveAvatar(keyPress, op);
+            }
+            
+        }
+
+        private static void MoveAvatar(ConsoleKey keyPress, Outputter op)
+        {
+            var outputchar = '0';
+
+            switch (keyPress)
+            {
+                case ConsoleKey.RightArrow:
+                    op.MoveFromLeft(1);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    op.MoveFromLeft(-1);
+                    break;
+                case ConsoleKey.UpArrow:
+                    op.MoveFromTop(-1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    op.MoveFromTop(1);
+                    break;
+            }
+
+            op.WC(outputchar);
+        }
+
+        private static ConsoleKey GetUserInput(Outputter op)
+        {
+            var keyPress = Console.ReadKey().Key;
+            op.MoveFromLeft(-1); // readkey verkar flytta markören till höger
+            return keyPress;
+        }
+
+
+        private static async Task<int> GetInt()
+        {
+            return 3;
+        }
+    }
+}
